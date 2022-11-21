e3faeb341c0766b7721a63fd86389964094b3dd3
stevebroll <sb2643@cornell.edu>
2022-11-21 12:18:56 -0500

    change outputs of get_delta functions to return stacked block design matrix for X and stacked response vector for Y. add scaling for X

--- R/get_delta.R
+++ R/get_delta.R
@@ -1,18 +1,34 @@
 
 
-get_delta_X <- function(X, t) {
-  DX <- array(NULL, dim = c(nrow(X), ncol(X), t - 1))
+get_delta_X <- function(X, t, n, p) {
+  DXarray <- array(0, dim = c(n, p, t - 1))
   for (i in 1:(t - 1)) {
-    DX[, , i] <- X[, , i + 1] - X[, , i]
+    DXarray[, , i] <- scale(X[, , i + 1] - X[, , i], center = F)/sqrt(n-1)
+  }
+
+  DX = matrix(0, nrow = n*(t-1), ncol = sum(1:(t-1)) * p)
+  rowvec = 1:n
+  colvec = 1:p
+  for(i in (t-1):1){
+    for(j in i:1){
+      DX[rowvec, colvec] = DXarray[,,j]
+      colvec = colvec + p
+    }
+    rowvec = rowvec + n
   }
   return(DX)
 }
 
 
 get_delta_Y <- function(Y, t) {
-  DY <- matrix(NULL, nrow = nrow(Y), ncol = t - 1)
+  DY <- rep(0, n*(t-1))
+  rowvec = 1:n
   for (i in 1:(t - 1)) {
-    DY[, i] <- Y[, i + 1] - Y[, i]
+    DY[rowvec] <- Y[, i + 1] - Y[, i]
+    rowvec = rowvec + n
   }
   return(DY)
 }
+
+
+
