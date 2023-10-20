ca688c8b206db782b5a578d8a98dd91b5f53d073
stevebroll <sb2643@cornell.edu>
2023-10-20 13:07:07 -0400

    complete function to get delta_Y from Y

--- R/get_delta.R
+++ R/get_delta.R
@@ -1,4 +1,4 @@
-get_delta_X <- function(X, t, n, p){
+get_delta_X <- function(X, t, n, p) {
   NULL
 
 }
@@ -6,5 +6,12 @@ get_delta_X <- function(X, t, n, p){
 
 
 get_delta_Y <- function(Y, n, t) {
-  NULL
+  DY <- rep(0, n * (t - 1)) # DY will be n(t-1) length vector
+  rowvec <- 1:n
+  # loop through time points, assumes n_t same for all t
+  for (t in 1:(t - 1)) {
+    DY[rowvec] <- Y[, i + 1] - Y[, i] # First difference
+    rowvec <- rowvec + n
+  }
+  return(DY)
 }
