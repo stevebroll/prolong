d16aa6f3d9a98b2ebc09a3f9b2197d3de7dadbce
stevebroll <sb2643@cornell.edu>
2023-10-20 14:50:18 -0400

    complete function to get correlation matrix from DXarray

--- R/get_cor_matrix.R
+++ R/get_cor_matrix.R
@@ -1,3 +1,13 @@
-get_cor_matrix <- function(DX, n, p, t) {
-
+get_cor_matrix <- function(DXarray, n, p, t) {
+  cormat <- matrix(0, nrow = sum(1:(t - 1)) * p, ncol = sum(1:(t - 1)) * p)
+  colvec = 0
+  for (i in 1:(t - 1)) {
+    DXtemp <- NULL
+    colvec = 1:(p * i) + max(colvec)
+    for (j in 1:i) {
+      DXtemp = cbind(DXtemp, DXarray[, , j])
+    }
+    cormat[colvec, colvec] = cor(DXtemp)
+  }
+  return(cormat)
 }
