624f38dc99a1a4cca9c4558d0c1b978c0673c8f6
stevebroll <sb2643@cornell.edu>
2023-10-20 14:57:49 -0400

    reformat, add DX,DY, and cormat calculations

--- R/prolong.R
+++ R/prolong.R
@@ -1,11 +1,30 @@
-prolong <- function(Y, X, lambda1 = NULL, lambda2 = NULL, groups = NULL) {
-  if (nrow(Y) != nrow(X)) stop("Incompatible dimensions for X and Y, X and Y should have same # rows, 1 for each sample")
-  if (ncol(Y) != dim(X)[3]) stop("Incompatible dimensions for X and Y, Y should have t columns the third component of dim(X) should also be t")
-  if (is.null(groups)) message("No groups supplied, ordinary lasso will be used instead of group lasso")
-  if(!is.null(groups) & groups != sort(groups)) stop("Groups must consist of consecutive columns, with group numbers counting from 1 to # groups")
-  n <- nrow(X)
-  p <- ncol(X)
-  t <- ncol(Y)
-  g <- length(unique(groups))
+prolong <-
+  function(Y,
+           X,
+           lambda1 = NULL,
+           lambda2 = NULL,
+           groups = NULL) {
+    if (nrow(Y) != nrow(X))
+      stop("Incompatible dimensions for X and Y, X and Y should have same # rows, 1 for each sample")
+    if (ncol(Y) != dim(X)[3])
+      stop(
+        "Incompatible dimensions for X and Y, Y should have t columns the third component of dim(X) should also be t"
+      )
+    if (is.null(groups))
+      message("No groups supplied, ordinary lasso will be used instead of group lasso")
+    if (!is.null(groups) &
+        groups != sort(groups))
+      stop(
+        "Groups must consist of consecutive columns, with group numbers counting from 1 to # groups"
+      )
+    n <- nrow(X)
+    p <- ncol(X)
+    t <- ncol(Y)
+    g <- length(unique(groups))
 
-}
+    DXout = get_delta_X(X, n, p, t)
+    DY = get_delta_Y(Y, n, t)
+    cormat = get_cor_matrix(DXout$DXarray)
+
+
+  }
