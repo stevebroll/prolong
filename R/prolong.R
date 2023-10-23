bc7184e8512ce037ab54dd142dbe5dcda14ba48e
stevebroll <sb2643@cornell.edu>
2023-10-23 13:30:28 -0400

    add optimization step for lambda2 and lambdar

--- R/prolong.R
+++ R/prolong.R
@@ -3,6 +3,7 @@ prolong <-
            X,
            lambda1 = NULL,
            lambda2 = NULL,
+           lambdar = NULL,
            groups = NULL) {
     if (nrow(Y) != nrow(X))
       stop("Incompatible dimensions for X and Y, X and Y should have same # rows, 1 for each sample")
@@ -22,9 +23,43 @@ prolong <-
     t <- ncol(Y)
     g <- length(unique(groups))
 
-    DXout = get_delta_X(X, n, p, t)
-    DY = get_delta_Y(Y, n, t)
-    cormat = get_cor_matrix(DXout$DXarray)
+    DXout <- get_delta_X(X, n, p, t)
+    DY <- get_delta_Y(Y, n, t)
+    cormat <- get_cor_matrix(DXout$DXarray)
+    graph <-
+      igraph::graph_from_adjacency_matrix(cormat,
+                                          mode = 'undirected',
+                                          weighted = T,
+                                          diag = F)
+    lap <-
+      as.matrix(igraph::laplacian_matrix(graph, normalized = T))
+
+    # Optimization for l2
+    if (is.null(lambda2) | is.null(lambdar)) {
+      message('lambda2 and/or lambdar missing, optimizing over both')
+      ZTZ = crossprod(DXout$DX)
+      ZTY = crossprod(DXout$DX, DY)
+      YZT = crossprod(DY, DXout$DX)
+
+      minfun =  function(l) {
+        n * log(crossprod(Y, Y) - YTZ %*% solve(abs(l[1]) * (Q + diag(l[2], nrow(
+          Q
+        )))
+        + ZTZ) %*% ZTY) + log(abs(det(abs(l[1]) *
+                                        (
+                                          Q + diag(l[2], nrow(Q))
+                                        )
+                                      + crossprod(Z, Z)))) - log(abs(det(abs(l[1]) *
+                                                                           (
+                                                                             Q + diag(l[2], nrow(Q))
+                                                                           ))))
+      }
+      opt = optim(c(1, 0.1), minfun)
+      print(opt)
+      lambda2 = opt$par[1]
+      lambdar = opt$par[2]
+    }
+
 
 
   }
