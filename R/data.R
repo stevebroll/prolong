7a6cf952fc6c73523a2b317d21654726980956b6
stevebroll <sb2643@cornell.edu>
2023-10-31 15:49:16 -0400

    add documentation for simulated data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Simulated Metabolite Data
+#'
+#' A 15x100x4 array containing 20 simulated target metabolites and 80 simulated
+#' noise metabolites across 15 observations and 4 time points. Intended for use
+#' in `prolong` package plotting functions or as predictors for `sim_outcome` in
+#' the `prolong()` function.
+#'
+#' @format ## `sim_metabs`
+#' A 3-D numeric array with n = 15 rows, p = 100 columns, and t = 4 slices
+
+"sim_metabs"
+
+#' Simulated Clinical Outcome Data
+#'
+#' A 15x4 matrix containing 20 simulated clinical outcome values. Intended for
+#' use as the response variable with predictors from `sim_metabs` in
+#' the `prolong()` function.
+#'
+#' @format ## `sim_outcome`
+#' A numeric matrix with n = 15 rows and t = 4 columns
+
+
+"sim_outcome"
